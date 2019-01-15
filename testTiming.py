7b55c58d8ac8677479c9042678ee4f26f3bf2e9d
Robert Nix <nix@earthling.net>
2019-01-15 08:26:57 -0600

    Add example of @contextmanager

--- testTiming.py
+++ testTiming.py
@@ -9,9 +9,23 @@ class Timer:
         self.start = time.perf_counter()
         return self
 
-    def __exit__(self, *args):
+    # def __exit__(self, *args):
+    def __exit__(self, exc_type, exc, exc_tb):
         self.end = time.perf_counter()
         self.interval =  self.end - self.start
+
+
+from contextlib import contextmanager
+
+@contextmanager
+def Timer2(*args, **kwds):
+    start_t = time.perf_counter()
+    try:
+        yield
+    finally:
+        end_t = time.perf_counter()
+        interval = end_t - start_t
+        print("Time = {:.03f}".format(interval))
 
 
 import requests
@@ -21,3 +35,7 @@ with Timer() as t:
     print("Returned Status: {}".format(conn.status_code))
 
 print("Request (pc) took {:.03f} sec.".format(t.interval))
+print("-" * 5)
+with Timer2():
+    conn = requests.get('https://www.google.com')
+    print("Returned Status: {}".format(conn.status_code))

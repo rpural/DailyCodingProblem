f9216022c1f4717b8127e0f03f3c84b64572ad23
Robert Nix <nix@earthling.net>
2019-02-12 15:11:35 -0600

    Additional changes

--- rectangles.py
+++ rectangles.py
@@ -22,3 +22,42 @@ sq2 = Square(6)
 print("area = {}, perimeter = {}".format(square.area(), square.perimeter()))
 print("area = {}, perimeter = {}".format(sq2.area(), sq2.perimeter()))
 
+
+class Cube(Square):
+    def surface_area(self):
+        face_area = super().area()
+        return face_area * 6
+
+    def volume(self):
+        face_area = super().area()
+        return face_area * self.length
+
+
+cube = Cube(3)
+print("surface area = {}, volume = {}".format(cube.surface_area(), cube.volume()))
+
+
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
+
+
+class RightPyramid(Square, Triangle):
+    def __init__(self, base, slant_height):
+        self.base = base
+        self.slant_height = slant_height
+        super().__init__(self.base)
+
+    def area(self):
+        base_area = super().area()
+        perimeter = super().perimeter()
+        return 0.5 * perimeter * self.slant_height + base_area
+
+
+pyr = RightPyramid(2, 4)
+print("pyramid area = {}".format(pyr.area()))
+

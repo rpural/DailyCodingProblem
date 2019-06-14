8064ca3e19cdc4bef6d2e0a1ff0aba03297f968f
Robert Nix <nix@earthling.net>
2019-06-14 14:42:50 -0400

    New daily coding problem

---
+++ Daily Coding Problem/flatten.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+
+''' Daily Coding Problem
+This problem was asked by Stripe.
+
+Write a function to flatten a nested dictionary. Namespace the keys with a period.
+
+For example, given the following dictionary:
+
+{
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+        }
+    }
+}
+it should become:
+
+{
+    "key": 3,
+    "foo.a": 5,
+    "foo.bar.baz": 8
+}
+You can assume keys do not contain dots in them, i.e. no clobbering will occur.
+'''
+
+initial = {
+    		"key": 3,
+    		"foo": {
+        		"a": 5,
+        		"bar": {
+            			"baz": 8
+        		}
+		}
+    	}
+
+print(initial)

ddff6c9d5467282049ff1502a87ca621b71995b3
Robert Nix <nix@earthling.net>
2019-11-11 16:30:32 -0500

    Card deck implementation

--- cards.py
+++ cards.py
@@ -2,11 +2,11 @@
 
 ''' A Cards class '''
 
-import collections
+import collections.abc
 
 Card = collections.namedtuple("Card", ['rank', 'suit'])
 
-class Deck:
+class Deck(collections.abc.MutableSequence):
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
 
@@ -20,7 +20,29 @@ class Deck:
     def __getitem__(self, position):
         return self._cards[position]
 
+    def __setitem__(self, position, card):
+        self._cards[position] = card
+
+    def __delitem__(self, position):
+        del self._cards[position]
+
+    def insert(self, position, value):
+        self._cards.insert(position, value)
+
 
 if __name__ == "__main__":
+    from random import shuffle
+
     deck = Deck()
+    print("size of deck is {}".format(len(deck)))
     print("fifth card is {}, tenth card is {}".format(deck[4], deck[9]))
+
+    shuffle(deck)
+    print("after shuffle:")
+    print("size of deck is {}".format(len(deck)))
+    print("fifth card is {}, tenth card is {}".format(deck[4], deck[9]))
+
+    deck.insert(52, Card(0, 'J'))
+    print("after adding Joker:")
+    print("size of deck is {}".format(len(deck)))
+    print("last cards are: {}, {}".format(deck[-2], deck[-1]))

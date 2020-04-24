65f1659d24386e4b0fae2b9a38bda05ba0365a13
Robert Nix <nix@earthling.net>
2020-04-24 13:11:33 -0400

    Add furlough date to the display

--- retirement.py
+++ retirement.py
@@ -8,6 +8,9 @@ import datetime
 
 today_ = datetime.date.today()
 retire = datetime.date(2020,5,31)
+furlough = datetime.date(2020,5,21)
 delta = retire - today_
+delta_short = furlough - today_
 
 print(f"{delta.days} days until retirement...")
+print(f"{delta_short.days} days until furlough...")
